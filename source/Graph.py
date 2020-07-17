0f5b3d30b57f23e6432054e96e71e1c889e6de30
lvjonok <kozlov.l.a@mail.ru>
2020-07-18 01:03:24 +0300

    new logic and generation

--- source/Graph.py
+++ source/Graph.py
@@ -1,6 +1,6 @@
 from random import randint, shuffle
 import sys
-sys.setrecursionlimit(50000)  
+# sys.setrecursionlimit(50000)  
 class Graph:
     def __init__(self, size_x, size_y):
         self.visited = []
@@ -11,15 +11,57 @@ class Graph:
     def getNextVertex(self, vertex, direction):
         next_vertex = vertex
         if direction == 0:
-            next_vertex -= self.size_x if next_vertex > self.size_x else 0
+            next_vertex -= self.size_x if vertex > self.size_x - 1 else 0
         elif direction == 1:
-            next_vertex += 1 if next_vertex % self.size_x != self.size_x - 1 else 0
+            next_vertex += 1 if vertex % self.size_x != self.size_x - 1 else 0
         elif direction == 2:
-            next_vertex += self.size_x if next_vertex < self.size_x * (self.size_y - 1) else 0
-        elif direction == 3:
-            next_vertex -= 1 if next_vertex % self.size_x != 0 else 0
-        return next_vertex if next_vertex != vertex else False
-    def generateGraph(self, vertex, flag = True):
+            next_vertex += self.size_x if vertex + self.size_x < self.size_x * self.size_y else 0
+        elif direction == 3:    
+            next_vertex -= 1 if vertex % self.size_x != 0 else 0
+        return next_vertex if next_vertex != vertex and 0 <= next_vertex < self.size_y * self.size_y else -1
+
+    def generateGraph(self, start_vertex, loops = True):
+        self.backtrackerAlgo(start_vertex)
+        if not loops:
+            return loops
+        vertices = []
+        for _ in range((self.size_x * self.size_x)//2.5):
+            vertex = randint(0, self.size_x * self.size_y - 1)
+            vertices.append(vertex)
+            direction = randint(0, 3)
+            next_vertex = self.getNextVertex(vertex, direction)
+            if next_vertex != -1:
+                try:
+                    self.map[vertex][next_vertex] = 1
+                    self.map[next_vertex][vertex] = 1
+                except:
+                    print(vertex, next_vertex)
+                    raise ZeroDivisionError(vertex, next_vertex)
+
+    def backtrackerAlgo(self, start_vertex):
+        queue = [start_vertex]
+        visited = [start_vertex]
+        while len(queue) != 0:
+            current_vertex = queue[len(queue) - 1] # current is last
+            directions = [0, 1, 2, 3]
+            vertices = []
+            for direction in directions:
+                vertex = self.getNextVertex(current_vertex, direction)
+                if vertex != -1 and not vertex in visited : vertices.append(vertex)
+            shuffle(vertices)
+            for next_vertex in vertices[:randint(1, 3)]:
+                try:
+                    self.map[current_vertex][next_vertex] = 1
+                    self.map[next_vertex][current_vertex] = 1
+                except:
+                    raise ZeroDivisionError(current_vertex, next_vertex)
+                visited.append(next_vertex)
+                queue.append(next_vertex)
+                # break
+            if queue[len(queue) - 1] == current_vertex:
+                queue.pop()
+
+    def loopGraph(self, vertex, flag = True):
         if vertex in self.visited:
             return True
         # print('entered', vertex)
@@ -27,32 +69,25 @@ class Graph:
         available = []
         for d in range(4):
             n_v = self.getNextVertex(vertex, d)
-            if n_v and not n_v in self.visited:
-                available.append(n_v)
+            # if n_v and not n_v in self.visited:
+            available.append(n_v)
         if len(available) != 0:
-            ways = randint(1, len(available))
+            # ways = randint(1, len(available))
             shuffle(available)
-            for way in range(ways):
+            for way in range(4):
                 selected_vertex = available[way]
+                if not selected_vertex: continue
                 if (not flag and not selected_vertex in self.visited) or flag:
                     self.map[vertex][selected_vertex] = 1
                     self.map[selected_vertex][vertex] = 1
                     self.generateGraph(selected_vertex, flag)
                 else:
-                    continue
+                    continue    
     def getMapVertexList(self):
         out = [[-1, -1, -1, -1] for vertex in range(self.size_x * self.size_y)]
         for vertex in range(self.size_x * self.size_y):
             for d in range(4):
                 nv = self.getNextVertex(vertex, d)
-                if nv:
+                if nv != -1:
                     out[vertex][d] = self.map[vertex][nv]
-        # for vi, vl in enumerate(out):
-        #     print(vi, vl)
         return out
-
-# g = Graph(5, 5)
-# g.generateGraph(randint(0, 24))
-# v = (g.getMapVertexList())
-# for vi, vl in enumerate(v):
-#     print(vi, vl)
