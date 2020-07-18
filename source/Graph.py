dd2ea87210849bc87c07515efb1dc77a11802af6
lvjonok <kozlov.l.a@mail.ru>
2020-07-18 10:58:14 +0300

    changed loops random tool

--- source/Graph.py
+++ source/Graph.py
@@ -18,25 +18,26 @@ class Graph:
             next_vertex += self.size_x if vertex + self.size_x < self.size_x * self.size_y else 0
         elif direction == 3:    
             next_vertex -= 1 if vertex % self.size_x != 0 else 0
-        return next_vertex if next_vertex != vertex and 0 <= next_vertex < self.size_y * self.size_y else -1
+        out = next_vertex if next_vertex != vertex and 0 <= next_vertex < self.size_y * self.size_y else -1
+        return out
 
     def generateGraph(self, start_vertex, loops = True):
         self.backtrackerAlgo(start_vertex)
         if not loops:
             return loops
-        vertices = []
-        for _ in range((self.size_x * self.size_x)//2.5):
-            vertex = randint(0, self.size_x * self.size_y - 1)
-            vertices.append(vertex)
-            direction = randint(0, 3)
-            next_vertex = self.getNextVertex(vertex, direction)
-            if next_vertex != -1:
-                try:
-                    self.map[vertex][next_vertex] = 1
-                    self.map[next_vertex][vertex] = 1
-                except:
-                    print(vertex, next_vertex)
-                    raise ZeroDivisionError(vertex, next_vertex)
+
+        walls = []
+
+        for v1 in range(self.size_x * self.size_y):
+            for v2 in range(v1 + 1, self.size_x * self.size_y):
+                if self.map[v1][v2] != 1:
+                    walls.append((v1, v2))
+
+        for _ in range(len(walls) // 5):
+            delete_index = randint(0, len(walls) - 1)
+            v1, v2 = walls[delete_index]
+            self.map[v1][v2] = 1
+            self.map[v2][v1] = 1
 
     def backtrackerAlgo(self, start_vertex):
         queue = [start_vertex]
@@ -49,7 +50,7 @@ class Graph:
                 vertex = self.getNextVertex(current_vertex, direction)
                 if vertex != -1 and not vertex in visited : vertices.append(vertex)
             shuffle(vertices)
-            for next_vertex in vertices[:randint(1, 3)]:
+            for next_vertex in vertices[:randint(1, 4)]:
                 try:
                     self.map[current_vertex][next_vertex] = 1
                     self.map[next_vertex][current_vertex] = 1
