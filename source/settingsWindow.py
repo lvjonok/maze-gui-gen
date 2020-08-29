183901ea18c28fa57d419d2f7edf0ea0fa5c9ede
lvjonok <kozlov.l.a@mail.ru>
2020-08-29 11:34:09 +0300

    old pull request change Avoid problems with native QColorDialog windows

--- source/settingsWindow.py
+++ source/settingsWindow.py
@@ -199,7 +199,7 @@ class SettingsWindow(QtWidgets.QWidget, settingsUI.Ui_settingsForm):
         return v
 
     def updateValueColorLine(self, event):
-        color = QtWidgets.QColorDialog.getColor()
+        color = QtWidgets.QColorDialog.getColor(options=QtWidgets.QColorDialog.DontUseNativeDialog)
         if color:
             rgb = color.getRgb()[0:3]
             hex_color = self.rgb_to_hex(rgb)
